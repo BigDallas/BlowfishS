33dd321a0a50cd4e2e502d88f013a574dcc6feff
BigDallas <68933044+BigDallas@users.noreply.github.com>
2025-10-29 01:53:14 -0400

    added wranf

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# @file
+# Builds a Hugo site hosted on a Cloudflare Worker.
+#
+# The Cloudflare Worker automatically installs Node.js dependencies.
+#------------------------------------------------------------------------------
+
+main() {
+
+  HUGO_VERSION=0.152.2
+
+
+  export TZ=Europe/Oslo
+
+
+"
+
+  # Install Hugo
+  echo "Installing Hugo ${HUGO_VERSION}..."
+  curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  mkdir "${HOME}/.local/hugo"
+  tar -C "${HOME}/.local/hugo" -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  rm "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  export PATH="${HOME}/.local/hugo:${PATH}"
+
+
+
+  # Verify installations
+  echo "Verifying installations..."
+  echo Go: "$(go version)"
+  echo Hugo: "$(hugo version)"
+  echo Node.js: "$(node --version)"
+
+  # Configure Git
+  echo "Configuring Git..."
+  git config core.quotepath false
+  if [ "$(git rev-parse --is-shallow-repository)" = "true" ]; then
+    git fetch --unshallow
+  fi
+
+  # clone themnes
+  echo "Clone Blowfish..."
+  git submodule update --init --recursive
+  git config core.quotepath false
+
+  # Build the site
+  echo "Building the site..."
+  hugo --gc --minify
+
+}
+
+set -euo pipefail
+main "$@"
